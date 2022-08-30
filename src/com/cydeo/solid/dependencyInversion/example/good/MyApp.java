4005a98b9ef5829119a1dc612e55fa97c6b92597
Bilal Ghani <bghani1995@gmail.com>
2022-08-30 14:49:47 -0500

    Dependency Inversion

---
+++ src/com/cydeo/solid/dependencyInversion/example/good/MyApp.java
@@ -0,0 +1,15 @@
+package com.cydeo.solid.dependencyInversion.example.good;
+
+public class MyApp {
+
+    public static void read(Reader reader) {
+
+
+        UserReader userReader = new UserReader(reader);
+
+        userReader.getUsername();
+
+
+
+    }
+}
