f1e89a7204c175d0afdc99765a115f1419680f59
Bilal Ghani <bghani1995@gmail.com>
2022-08-30 11:42:56 -0500

    CRUD Service Class Created

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -0,0 +1,24 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class CRUDService {
+
+    public static void create(){
+        // Create a user
+        // Send it to database related part
+    }
+
+    public static void read(){
+        // Read a user
+        // Send it to database related part
+    }
+
+    public static void update(){
+        // Update a user
+        // Send it to database related part
+    }
+
+    public static void delete(){
+        // Delete a user
+        // Send it to database related part
+    }
+}
